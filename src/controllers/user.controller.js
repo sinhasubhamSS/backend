22d1cd2c3bd13f6e1c5d208b2c0bc02514ae8b14
sinhasubhamSS <subhamsinhass.344@gmail.com>
2024-08-02 15:20:15 +0530

    d lcomiit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,39 +33,55 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All Fields Are Required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
+  console.log(req.files);
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverimage[0]?.path;
-
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+  // console.log(avatarLocalPath);
+  // console.log("checking 1",coverImageLocalPath);
   if (!avatarLocalPath) {
     throw new ApiError(100, "Avatar is required");
   }
   const avatar = await uploadOnCloudinary(avatarLocalPath);
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-
+  // console.log(avatar);
+  const coverImage = coverImageLocalPath
+    ? await uploadOnCloudinary(coverImageLocalPath)
+    : null;
+  // console.log("Avatar upload result: ", avatar);
+  // console.log("Cover image upload result:", coverImage);
   if (!avatar) {
     throw new ApiError(100, "Avatar is required");
   }
-  const user=await User.create({
+  const user = await User.create({
     fullName,
     avatar: avatar.url,
-    coverImage:coverImage.url || "",
+    coverImage: coverImage?.url || "",
     email,
-    username:username.toLowerCase(),
+    password,
+    username: username.toLowerCase(),
   });
-  const createrUser=await User.findById(user._id).select(
+  const createrUser = await User.findById(user._id).select(
     "-password-refreshToken"
-  )
-  if(!createrUser){
-    throw new ApiError(500,"Sometthing went wrong while regitering the user")
+  );
+  if (!createrUser) {
+    throw new ApiError(500, "Sometthing went wrong while regitering the user");
   }
-  return res.status(201).json(
-    new ApiResponse(200,createrUser,"User registered succesffully")
-  )
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createrUser, "User registered succesffully"));
 });
+const loginUser=asyncHandler(async(req,res))
 export { registerUser };
